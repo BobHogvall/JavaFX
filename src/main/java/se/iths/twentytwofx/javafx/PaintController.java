cecc98ef1f78725a092e6860296d201e27592b63
bobster <bob.hogvall@iths.se>
2023-09-01 12:30:54 +0200

    add to controller class and view

--- src/main/java/se/iths/twentytwofx/javafx/PaintController.java
+++ src/main/java/se/iths/twentytwofx/javafx/PaintController.java
@@ -1,5 +1,6 @@
 package se.iths.twentytwofx.javafx;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.control.Button;
@@ -13,19 +14,44 @@ public class PaintController {
 
     public MenuBar menuBar;
     public Button circle;
-    public Button Rectangle;
+    public Button rectangle;
     public ColorPicker colorPicker;
     public Button undo;
     public Button redo;
     public Button save;
     public Canvas canvas;
 
-
-
+    public void menuBarAction(){
+        //todo: vilka actions ska finnas här, ska vi ens använda den?
+        //todo: metoder för menuBar + items?
+    }
+    public void circleAction(ActionEvent actionEvent){
+        // todo: metod för att skapa cirkel
+    }
+    public void rectangleAction(ActionEvent actionEvent){
+        // todo: metod för att skapa rektangel
+    }
+    public void colorPickerAction(){
+        // todo: metod för att välja färg
+    }
+    public void undoAction(ActionEvent actionEvent){
+        //todo: metod för att radera föregående, ev flera
+    }
+    public void redoAction(ActionEvent actionEvent){
+        //todo: metod för att gå tillbaka, ev flera
+    }
+    public void saveAction(ActionEvent actionEvent){
+        //todo: metod för att spara
+    }
+    public void canvasAction(){
+        //todo: metod för att rita på canvas
+    }
 
 
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
     }
+
 }
+
